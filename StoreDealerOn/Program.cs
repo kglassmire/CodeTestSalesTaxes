a7413ffb822702df9508908022970e76b9c5152b
Kristopher Glassmire <kglassmire@gmail.com>
2019-03-26 22:59:13 -0400

    Add project files.

---
+++ StoreDealerOn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreDealerOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
